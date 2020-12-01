84251a1f41d5a0357ee7b649e5fce9de0cfdc87a
Marlon Yepes <protectfacebook.php@gmail.com>
2020-12-01 16:47:52 -0500

    websocket server is done

--- cursonode/src/index.js
+++ cursonode/src/index.js
@@ -2,7 +2,8 @@ import jwt_middleware  from './middleware/auth-jwt'
 const express = require('express')
 const cors = require('cors')
 const app  = express(); 
-//const path = require('path')
+const server = require('http').Server(app)
+const socket = require('./socket')
 const bodyParser = require('body-parser')
 const db  = require('./database/db')
 
@@ -16,20 +17,16 @@ app.use(cors({
 app.set('PORT', 4000); //Guarda datos en memoria (LLAVE, VALOR)
 app.use(bodyParser.json())//Todas las peticiones se transforman en json
 app.use(bodyParser.urlencoded({extended: false}))//Recibir url complejas
-
-
-app.get('/', (request, response ) => {
-    response.status(200).sendFile(path.join(__dirname, 'index.html'))
-})
-
 app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes) //UTILIZAR RUTA USERROUTES (API)
+app.use('/api/user' , jwt_middleware, userRoutes) //UTILIZAR RUTA USERROUTES (API)
 
 
 
-const server = app.listen(app.get('PORT'), ()=> { //Nos "devuelve" un servidor en la constante server, Crear server
+server.listen(app.get('PORT'), ()=> { //Nos "devuelve" un servidor en la constante server, Crear server
     console.log(`App   on  port ${app.get('PORT')}`)
     db.connect()
     .then(res => console.log(res))
-    .catch(error =>  console.log(error))
+    .catch(error =>  console.log(error)); 
+
+    socket.connect(server)
 })
